413fbad8a7a5200e90d190174e605b6a5c511c6f
CodeZwei <brandonzwei@gmail.com>
2020-11-17 23:28:58 -0800

    Imported mod script and exported assets

--- mod/mod.js
+++ mod/mod.js
@@ -2,8 +2,24 @@
   const mod = new Mod("nidalee-skin");
   mod.name = "Nidalee Outfit";
 
+  mod.loadImage('Queen-Clothes-NidaleeQueen', 'assets/Queen-Clothes-NidaleeQueen.png');
+  mod.loadImage('Queen-Clothes-NidaleeQueen-Exposed', 'assets/Queen-Clothes-NidaleeQueen-Exposed.png');
+  mod.loadImage('Queen-Clothes-NidaleeQueen-Naked', 'assets/Queen-Clothes-NidaleeQueen-Naked.png');
+  mod.loadImage('NidaleeQueen', 'assets/NidaleeQueen.png');
+
   mod.init = () => {
     // Add setup code here
+    let clothes = new Clothes('NidaleeQueen', 'Queen', false)
+        .setName('Nidalee Outit')
+        .setDescription("Tribal costume inspired by Nidalee from League of Legends")
+        .setShop(true)
+        .setCost(100)
+        .setVisible(true)
+        .setLevel(0)
+        .setStat({Throat: 2, Tits: 2, Pussy: 3})
+        .addStyle(new ClothesStyle('Exposed', 'Exposed', true))
+        .addStyle(new ClothesStyle('Naked', 'Naked', true));
+    mod.clothes.add(clothes);
   };
 
   mod.boot = () => {
